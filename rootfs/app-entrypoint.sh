f9f5eb782c5b27ba02ab8f24eca6aa45ca2dc14c
dgonzalez <dgonzalez@bitnami.com>
2016-08-25 11:12:29 +0200

    Refactored entrypoint behaviour

--- rootfs/app-entrypoint.sh
+++ rootfs/app-entrypoint.sh
@@ -24,19 +24,17 @@ log () {
     echo -e "\033[0;33m$(date "+%H:%M:%S")\033[0;37m ==> $1."
 }
 
-if [ "$1" == "activator" -a "$2" == "run" ] ; then
+if [ "$1" == "activator" -a "$2" == "~run" ] ; then
     if [ ! -d $PROJECT_DIRECTORY ] ; then
-	  log "Creating example Play application"
+      log "Creating example Play application"
       cd /app
       activator new $PLAY_PROJECT_NAME $PLAY_TEMPLATE
       log "Play app created"
+      cd $PLAY_PROJECT_NAME
     else 
 	  log "App already created"
-      cd $PROJECT_DIRECTORY 
+      cd $PROJECT_DIRECTORY
     fi
 fi
-
-# Making sure we end up in the project directory
-cd $PROJECT_DIRECTORY
 
 exec /entrypoint.sh "$@"
